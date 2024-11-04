10a4574f857d820d35ce089e5c67bc6d3f21334c
Tianyi Yang <thierry.yang2002@gmail.com>
2024-11-04 17:56:04 +0000

    Ajoute plusieurs astéroïdes, ajoute la logique de collision

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -6,20 +6,37 @@ use ::rand::{thread_rng, Rng};
 pub struct Asteroid {
     position : Vec2,
     speed : Vec2,
+    size: f32,
 }
 
 impl Asteroid {
-    pub const ASTEROID_INIT_SIZE : f32 = 60.0;
+    pub const LARGE: f32 = 60.0;
+    pub const MEDIUM: f32 = 30.0;
+    pub const SMALL: f32 = 10.0;
 
     pub fn new() -> Self {
         Self {
             position: Self::new_alea_pos(),
-            speed : Self::new_alea_speed(),
+            speed: Self::new_alea_speed(),
+            size: Self::LARGE,
+        }
+    }
+
+    //Créer des astéroïdes de taille connue à l'endroit souhaité.
+    pub fn with_size(size: f32, position: Vec2) -> Self {
+        Self {
+            position,
+            speed: Self::new_alea_speed(),
+            size,
         }
     }
 
     pub fn get_position(&self) -> Vec2 {
         self.position
+    }
+
+    pub fn get_size(&self) -> f32 {
+        self.size
     }
 
     pub fn move_object(&mut self) -> Vec2{
@@ -28,11 +45,24 @@ impl Asteroid {
         self.position
     }
 
+    pub fn split(&self) -> Vec<Asteroid> {
+        let new_size = match self.size {
+            Self::LARGE => Self::MEDIUM,
+            Self::MEDIUM => Self::SMALL,
+            _ => return vec![], 
+        };
+
+        vec![
+            Asteroid::with_size(new_size, self.position + vec2(20.0, 20.0)),
+            Asteroid::with_size(new_size, self.position + vec2(-20.0, -20.0)),
+        ]
+    }
+
     /// Génère une position aléatoire près de l'un des bords.
     fn new_alea_pos() -> Vec2 {
         let mut rng = thread_rng();
 
-        let nearpos : f32 = rng.gen_range(Self::ASTEROID_INIT_SIZE/2.0..=Self::ASTEROID_INIT_SIZE);
+        let nearpos : f32 = rng.gen_range(Self::LARGE/2.0..=Self::LARGE);
         let nearside = rng.gen_range(1..=4); // 1 = top, 2 = right, 3 = down, 4 = left
         let xpos : f32 = match nearside {
             2 => screen_width() - nearpos,
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ use macroquad::prelude::*;
 
 mod asteroid;
 
-fn draw(asteroid : &Asteroid) {
+fn draw(asteroids: &[Asteroid]) {
     draw_background();
-    draw_asteroids(asteroid);
+    draw_asteroids(asteroids);
 }
 
 fn draw_background() {
     clear_background(BLACK);
 }
 
-fn draw_asteroids(asteroid : &Asteroid) {
-    draw_circle_lines(asteroid.get_position().x, asteroid.get_position().y, 60.0, 1.0, YELLOW);
+fn draw_asteroids(asteroids: &[Asteroid]) {
+    for asteroid in asteroids {
+        draw_circle_lines(
+            asteroid.get_position().x,
+            asteroid.get_position().y,
+            asteroid.get_size() / 2.0,
+            1.0,
+            YELLOW,
+        );
+    }
 }
 
 fn handle_input() -> bool {
@@ -24,20 +32,74 @@ fn handle_input() -> bool {
     false
 }
 
-fn update_model(asteroid : &mut Asteroid) {
-    asteroid.move_object();
+fn update_model(asteroids: &mut [Asteroid]) {
+    for asteroid in asteroids {
+        asteroid.move_object();
+    }
 }
+
+//La logique de collision a été modifiée : si deux astéroïdes entrent en collision, 
+//ceux de même taille se divisent, tandis que si leur taille est différente, 
+//c'est le plus petit qui se divise.
+fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
+    let mut new_asteroids = vec![];
+    let mut to_remove = vec![]; 
+
+    for i in 0..asteroids.len() {
+        for j in (i + 1)..asteroids.len() {
+            let asteroid_a = &asteroids[i];
+            let asteroid_b = &asteroids[j];
+
+            let distance = asteroid_a.get_position().distance(asteroid_b.get_position());
+
+            if distance < (asteroid_a.get_size() + asteroid_b.get_size()) / 2.0 {
+                if (asteroid_a.get_size() - asteroid_b.get_size()).abs() < f32::EPSILON {  // =0 possible pose problem float
+                    to_remove.push(i);
+                    to_remove.push(j);
+                    new_asteroids.extend(asteroid_a.split());
+                    new_asteroids.extend(asteroid_b.split());
+                } else {
+                    let (small_idx, small_asteroid) = if asteroid_a.get_size() < asteroid_b.get_size() {
+                        (i, asteroid_a)
+                    } else {
+                        (j, asteroid_b)
+                    };
+
+                    to_remove.push(small_idx);
+                    new_asteroids.extend(small_asteroid.split());
+                }
+                break;
+            }
+        }
+    }
+
+    to_remove.sort_unstable();
+    to_remove.dedup();
+    for &index in to_remove.iter().rev() {
+        asteroids.remove(index);
+    }
+
+    asteroids.extend(new_asteroids);
+}
+
+
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let mut asteroid = asteroid::Asteroid::new();
+    let mut asteroids = Vec::new();
+
+    for _ in 0..10 {
+        asteroids.push(asteroid::Asteroid::new());
+    }
 
     loop {
-        draw(&asteroid);
+        draw(&asteroids);
 
         if handle_input() { break; }
 
-        update_model(&mut asteroid);
+        update_model(&mut asteroids);
+
+        handle_collisions(&mut asteroids);
 
         next_frame().await
     }
