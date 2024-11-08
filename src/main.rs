2ec7258f4984c4166296840b62dec67f883a0f4d
tianyi.yang <tianyi.yang@etu.unistra.fr>
2024-11-08 20:49:37 +0100

    La classe spaceship a été créée et utilisée dans le programme principal, et certaines fonctions ont été modifiées pour permettre à spaceship et astéroïde de utiliser ensemble

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use asteroid::Asteroid;
+use spaceship::Spaceship;
 use macroquad::prelude::*;
 
 mod asteroid;
+mod spaceship; 
 
-fn draw(asteroids: &[Asteroid]) {
+
+fn draw(asteroids: &[Asteroid], spaceship: &Spaceship) {
     draw_background();
     draw_asteroids(asteroids);
+    spaceship.draw(); 
 }
 
 fn draw_background() {
@@ -24,26 +28,35 @@ fn draw_asteroids(asteroids: &[Asteroid]) {
     }
 }
 
-fn handle_input() -> bool {
+fn handle_input(spaceship: &mut Spaceship) -> bool {
     if is_key_down(KeyCode::Escape) {
-        return true
+        return true;
     }
+
+    if is_key_down(KeyCode::Left) {
+        spaceship.rotate_left();
+    }
+    if is_key_down(KeyCode::Right) {
+        spaceship.rotate_right();
+    }
+    spaceship.set_push(is_key_down(KeyCode::Up));
 
     false
 }
 
-fn update_model(asteroids: &mut [Asteroid]) {
+fn update_model(asteroids: &mut [Asteroid], spaceship: &mut Spaceship) {
     for asteroid in asteroids {
         asteroid.move_object();
     }
+    spaceship.update();
 }
 
 //La logique de collision a été modifiée : si deux astéroïdes entrent en collision, 
 //ceux de même taille se divisent, tandis que si leur taille est différente, 
 //c'est le plus petit qui se divise.
-fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
+fn handle_collisions(asteroids: &mut Vec<Asteroid>, spaceship: &Spaceship) -> bool {
     let mut new_asteroids = vec![];
-    let mut to_remove = vec![]; 
+    let mut to_remove = vec![];
 
     for i in 0..asteroids.len() {
         for j in (i + 1)..asteroids.len() {
@@ -51,9 +64,8 @@ fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
             let asteroid_b = &asteroids[j];
 
             let distance = asteroid_a.get_position().distance(asteroid_b.get_position());
-
             if distance < (asteroid_a.get_size() + asteroid_b.get_size()) / 2.0 {
-                if (asteroid_a.get_size() - asteroid_b.get_size()).abs() < f32::EPSILON {  // =0 possible pose problem float
+                if (asteroid_a.get_size() - asteroid_b.get_size()).abs() < f32::EPSILON {
                     to_remove.push(i);
                     to_remove.push(j);
                     new_asteroids.extend(asteroid_a.split());
@@ -64,7 +76,6 @@ fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
                     } else {
                         (j, asteroid_b)
                     };
-
                     to_remove.push(small_idx);
                     new_asteroids.extend(small_asteroid.split());
                 }
@@ -73,13 +84,25 @@ fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
         }
     }
 
+    for (_i, asteroid) in asteroids.iter().enumerate() {
+        let distance = asteroid.get_position().distance(spaceship.get_position());
+        if distance < asteroid.get_size() / 2.0 + Spaceship::SIZE / 2.0 {
+            println!("Game Over!"); 
+            return false;      //Déboguer le programme en désactivant temporairement la fin du jeu en cas de collision.
+        }
+    }
+
     to_remove.sort_unstable();
     to_remove.dedup();
+
     for &index in to_remove.iter().rev() {
-        asteroids.remove(index);
+        if index < asteroids.len() { 
+            asteroids.remove(index);
+        }
     }
 
     asteroids.extend(new_asteroids);
+    false
 }
 
 
@@ -87,19 +110,22 @@ fn handle_collisions(asteroids: &mut Vec<Asteroid>) {
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let mut asteroids = Vec::new();
+    let mut spaceship = Spaceship::new();
 
-    for _ in 0..10 {
+    for _ in 0..100 {
         asteroids.push(asteroid::Asteroid::new());
     }
 
     loop {
-        draw(&asteroids);
+        draw(&asteroids, &spaceship);
 
-        if handle_input() { break; }
+        if handle_input(&mut spaceship) { break; }
 
-        update_model(&mut asteroids);
+        update_model(&mut asteroids, &mut spaceship);
 
-        handle_collisions(&mut asteroids);
+        if handle_collisions(&mut asteroids, &spaceship) {
+            break;
+        }
 
         next_frame().await
     }
